7cebc95efa96954854072f26b7f1f04b24ceec43
SpaceNoob123 <markusgame31@gmail.com>
2023-03-25 13:07:08 +0200

    add new buttons
    Left button change background on red
    Right button change background on green

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
@@ -34,8 +35,40 @@ namespace WindowsFormsApp1
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
-            this.BackColor = Color.Red;
+            this.BackColor = Color.Yellow;
+            this.Size = new Size(600, 625);
+            this.Location = new Point(500, 500);
+            btnred = new Button();
+            btnred.Text = "Red";
+            btnred.ForeColor = Color.Green;
+            btnred.Size = new Size(90, 100);
+            btnred.Location = new Point(0, 250);
+            btnred.BackColor = Color.Aquamarine;
+            btnred.Font = new Font("Times New Roman", 13, FontStyle.Bold | FontStyle.Italic);
+            btnred.Click += (e, sender) =>
+            {
+                this.BackColor = Color.Red;
+                btnred.Visible = true;
+            };
+
+            btngreen = new Button();
+            btngreen.Text = "Green";
+            btngreen.ForeColor = Color.Green;
+            btngreen.Size = new Size(90, 100);
+            btngreen.Location = new Point(500, 250);
+            btngreen.BackColor = Color.Aquamarine;
+            btngreen.Font = new Font("Times New Roman", 13, FontStyle.Bold | FontStyle.Italic);
+            btngreen.Click += (e, sender) =>
+            {
+                this.BackColor = Color.Green;
+                btngreen.Visible = true;
+            };
+
+            this.Controls.Add(btnred);
+            this.Controls.Add(btngreen);
         }
+        Button btnred;
+        Button btngreen;
 
         #endregion
     }
